4fd50d5f452072ef3b6f9fad6d19fbc72f2e3673
Daniel Monteagudo <dmonte3210@gmail.com>
2020-12-03 20:34:50 -0500

    updated code to include part 1

--- day1/day1.py
+++ day1/day1.py
@@ -2,11 +2,21 @@ with open('input', 'r') as expense_report:
     expenses = [ int(a.strip()) for a in expense_report.readlines() ]
     expenses = sorted(expenses)
 
-for a in expenses:
-    for b in expenses:
-        for c in expenses:
-            if a + b + c == 2020:
-                print(f"{a} + {b} + {c} = 2020, {a} * {b} * {c} = {a * b * c}")
-                exit()
+def part1():
+    for a in expenses:
+        for b in expenses:
+            if a + b  == 2020:
+                print(f"{a} + {b} = 2020, {a} * {b} = {a * b}")
+                return(a * b)
+                    
+def part2():
+    for a in expenses:
+        for b in expenses:
+            for c in expenses:
+                if a + b + c == 2020:
+                    print(f"{a} + {b} + {c} = 2020, {a} * {b} * {c} = {a * b * c}")
+                    return(a * b * c)
 
                 
+part1()
+part2()
